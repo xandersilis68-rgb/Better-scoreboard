99f2bc294823429ac46a45b4299a770355879a1f
xandersilis68-rgb <xander.silis68@schools.sa.edu.au>
2025-09-09 14:26:00 +0930

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Fill in your Firebase project config, then deploy to GitHub Pages.
-// See README.md for step-by-step instructions.
+// firebase-config.js
 window.FB_CFG = {
-  // apiKey: "YOUR_API_KEY",
-  // authDomain: "YOUR_PROJECT.firebaseapp.com",
-  // projectId: "YOUR_PROJECT_ID",
-  // storageBucket: "YOUR_PROJECT.appspot.com",
-  // messagingSenderId: "YOUR_SENDER_ID",
-  // appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAE-c2AegD0_sQ_UlX1XPdU8i89a8RwYc4",
+  authDomain: "very-f5d09.firebaseapp.com",
+  projectId: "very-f5d09",
+  storageBucket: "very-f5d09.firebasestorage.app",
+  messagingSenderId: "341853561851",
+  appId: "1:341853561851:web:6e25b9c870c2af4eafa15e",
+  measurementId: "G-GZJYH7JKB9"
 };
