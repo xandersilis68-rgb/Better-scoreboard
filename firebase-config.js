c129cfe53a207c836281eda97a3a20345dcd0269
xandersilis68-rgb <xander.silis68@schools.sa.edu.au>
2025-09-09 15:12:56 +0930

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.FB_CFG = {
   apiKey: "AIzaSyAE-c2AegD0_sQ_UlX1XPdU8i89a8RwYc4",
   authDomain: "very-f5d09.firebaseapp.com",
   projectId: "very-f5d09",
